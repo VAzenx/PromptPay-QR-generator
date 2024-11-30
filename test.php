5546f9a2a9e4751572e96f1779d9a6dcb44efa8b
VAzenx <145928575+VAzenx@users.noreply.github.com>
2024-11-30 23:46:08 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,6 @@ require_once("lib/PromptPayQR.php");
 
 $PromptPayQR = new PromptPayQR(); // new object
 $PromptPayQR->size = 8; // Set QR code size to 8
-$PromptPayQR->id = '0841079779'; // PromptPay ID
+$PromptPayQR->id = '0940173161'; // PromptPay ID
 $PromptPayQR->amount = 200.25; // Set amount (not necessary)
 echo '<img src="' . $PromptPayQR->generate() . '" />';
